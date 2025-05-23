c23642efc8363eea91a1e137a04b0e8d5fe0d787
Praveen <praveen@epikso.com>
2025-05-23 16:09:11 +0530

    Update home page dynamic

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://epikaws.online/v2qaapi/";
+export const BASE_URL = "https://epikaws.online/trackingapi/";
 // export const BASE_URL = "https://epikaws.online/emptrackingapi/";
 // export const BASE_URL = "http://localhost:8080/";
 
